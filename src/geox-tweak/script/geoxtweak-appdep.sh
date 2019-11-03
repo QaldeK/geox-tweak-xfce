bcedd45af23353e7ba4d17ee6a766009f2c4700a
qaldek <aldek@vivaldi.net>
2019-11-04 00:47:52 +0100

    Update GeoX-Tweak, geox-tweak.deb, and 14 more files...

--- src/geox-tweak/script/geoxtweak-appdep.sh
+++ src/geox-tweak/script/geoxtweak-appdep.sh
@@ -4,93 +4,160 @@
 
 dir=$(dirname "$0")  # 'dirname $0' renvoie le nom du répertoire dans lequel le script actuel se trouve.
 
-#Interface graphique YAD
-if GUI=$(yad --text="Les logiciels suivants vont être installé : 
+#Check lla distribution linux
+if [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    os=$DISTRIB_ID
+    ver=$DISTRIB_RELEASE
+fi
 
-- Plank : Une barre de lancement d'applications
-- Xfdashboard : Affiche les applications et fenetres en cours
-- Synapse : Lanceur d'applications rapide (raccourci Ctrl-espace)
-- Conky : Permet d'afficher divers informations sur le bureau (date, heure,
-ressources PCU, RAM...)
-- Redshift : Permet de moins se fatiguer les yeux
-- Collection de theme GTK et icones (Arc, Adapta, Materia...) "
+distrib=$os$ver
 
-)
+mainFunction () {
+	title="${1}"
+	command="${2}"
+	log="/tmp/$(date +%s)"
+	config="--progress --pulsate --center --no-buttons --auto-close
+	--progress-text='Installation ...' --width=600"
 
-then 
-	#plank
-	
-	which plank > /dev/null
-	if [ $? = 1 ]
-	then
-		sudo apt -y install plank
-		notify-send -i system-software-update "Installation de Plank" -t 5000
-	
-	fi
-	
-	#Xfdashboard
-	
-	which xfdashboard > /dev/null
-	if [ $? = 1 ]
-	then
-		notify-send -i system-software-update "Installation de xfdashboard" -t 5000
-		sudo apt -y install xfdashboard
-		sudo apt -y install xfdashboard-plugins
-	fi
-	
-	
-	
-	#Synapse
-	
-	which synapse > /dev/null
-	if [ $? = 1 ]
-	then
-		notify-send -i system-software-update "Installation de synapse" -t 5000
-		sudo apt -y install synapse
-	fi
-	
-	
-	#Redshift
-	
-	which redshift > /dev/null
-	if [ $? = 1 ]
-	then
-		notify-send -i system-software-update "Installation de redshift" -t 5000
-		sudo apt -y install redshift
-		sudo apt -y install redshift-gtk
+	${command} 2> "${log}" |
+	while read -r line; do echo "# ${line}"; done |
+	yad ${config} --title="${title}"
 
-	fi
+	checkErrorLog "${log}"
+}
 
-	#Conky
-	
-	witch conky > /dev/null
-	if [ $? = 1 ]
-	then
-		notify-send -i system-software-update "Installation de conky" -t 5000
-		sudo apt -y install conky-all
-		sudo apt -y install conky-manager
-	fi 
 
+checkErrorLog () {
+    log="${1}"
+    error=$(cat "${log}")
+    rm "${log}"
+
+    if [ "${error}" != "" ]; then
+        echo "${error}" >&2
+        errorWindow "${error}"
+        exit 1
+    else yad --text=" installation terminée " --button=OK:1
+    fi
+}
+
+
+errorWindow () {
+    error="${1}"
+    config="--center --button=OK:1"
+
+    yad ${config} --title="Error" --text="${error}" --width 800 --height 500
+}
+
+installation() 
+{ 
+		#plank
+		which plank > /dev/null
+		if [ $? = 1 ]
+		then
+			sudo apt-get install -y plank
+		fi
+		
+		#Xfdashboard
+		which xfdashboard > /dev/null
+		if [ $? = 1 ]
+		then
+			sudo apt-get install -y xfdashboard
+			sudo apt-get install -y xfdashboard-plugins
+		fi
+				
+		#Synapse
+		which synapse > /dev/null
+		if [ $? = 1 ]
+		then
+			sudo apt-get install -y synapse
+		fi
+		
+		
+		#Redshift
+		which redshift > /dev/null
+		if [ $? = 1 ]
+		then
+			sudo apt-get install -y redshift
+			sudo apt-get install -y redshift-gtk
+		fi
+
+		#Conky
+		which conky > /dev/null
+		if [ $? = 1 ]
+		then
+			sudo apt-get install -y conky-all
+			sudo apt-get install -y conky-manager
+		fi 
+		
+		#Dockbarx & xfce4-dockbarx-plugin
+		which dockx >/dev/null
+		if [ $? = 1 ] ;
+		then
+			#for mx 19
+			if [ $distrib = MX19 ] ; then
+			sudo sh -c "echo 'deb
+			http://ppa.launchpad.net/xuzhen666/dockbarx/ubuntu disco main' >
+			/etc/apt/sources.list.d/xuzhen666-ubuntu-dockbarx-disco.list"
+			sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com  77d026e2eead66bd 
+			sudo apt-get update
+			sudo apt-get install -y xfce4-dockbarx-plugin
+			sudo apt-get install -y dockbarx-themes-extra 
+			
+			#for ubuntu and derivative
+			elif [ $distrib = MX18 ] ; then
+			sudo apt-get install -y xfce4-dockbarx-plugin
+			sudo apt-get install -y dockbarx-themes-extra 
+
+			elif [ $os = Ubuntu ] ; then
+			sudo add-apt-repository ppa:xuzhen666/dockbarx
+			sudo apt-get update
+			sudo apt-get install -y xfce4-dockbarx-plugin
+			sudo apt-get install -y dockbarx-themes-extra 
+			
+			elif [ $os = LinuxMint ] ; then
+			sudo add-apt-repository ppa:xuzhen666/dockbarx
+			sudo apt-get update
+			sudo apt-get install -y xfce4-dockbarx-plugin
+			sudo apt-get install -y dockbarx-themes-extra 
+			
+			fi	
+		fi
 	### THEME ###
-	
 	# Arc
-	#notify-send -i system-software-update "Installation du theme Arc" -t 5000
-	#sudo apt -y install arc-theme arc-theme-hdpi arc-theme-xhdpi arc-grey-theme
-	
+	#sudo apt-get install -y arc-theme arc-theme-hdpi arc-theme-xhdpi arc-grey-theme
 	# Adapta
-	#notify-send -i system-software-update "Installation du theme Adapta" -t 5000
-	#sudo apt -y install adapta-gtk-theme adapta-gtk-theme-colorpack
+	#sudo apt-get install -y adapta-gtk-theme adapta-gtk-theme-colorpack
 	
 	
 	# Papirus icon
-	notify-send -i system-software-update "Installation du theme d'icones papyrus" -t 5000
-	sudo apt -y install papirus-icon-theme papirus-folders
+	sudo apt-get install -y papirus-icon-theme papirus-folders
 	
 	# copie des themes
-	sudo tar -zxfk theme.tar.gz -C /usr/share/themes/
+	#sudo tar -zxfk theme.tar.gz -C /usr/share/themes/
 	
-fi
+}
 
-yad --text="installation terminée"
+#Interface graphique YAD
+yadgui() {
+
+ GUI=$(yad --width=500 --heigth=600 \
+	--text="Les logiciels suivants vont être installé : 
+
+	- Plank : Une barre de lancement d'applications
+	- Xfdashboard : Affiche les applications et fenetres en cours
+	- Synapse : Lanceur d'applications rapide (raccourci Ctrl-espace)
+	- Conky : Permet d'afficher divers informations sur le bureau (date, heure,
+	ressources PCU, RAM...)
+	- Redshift : Permet de moins se fatiguer les yeux
+	- Collection de theme GTK et icones (Arc, Adapta, Materia...)"  \
+	)
+	
+	mainFunction "Installation" installation
+}
+
+yadgui
+
 
 
