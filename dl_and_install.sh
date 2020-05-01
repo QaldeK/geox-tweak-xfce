9e3f531bf59ab01031b07d8eca5e7968acd9b69d
QaldeK <aldek@vivaldi.net>
2020-05-01 09:47:24 +0000

    update dl_and-install

--- dl_and_install.sh
+++ dl_and_install.sh
@@ -2,13 +2,10 @@
 
 gh_repo="geox-tweak-xfce"
 temp_dir="$(mktemp -d)"
-
 echo "=> Getting the latest version from GitHub ..."
 wget -O "/tmp/$gh_repo.tar.gz" https://github.com/QaldeK/geox-tweak-xfce/archive/master.tar.gz
-
 echo "=> Unpacking archive ..."
 tar -xzf "/tmp/$gh_repo.tar.gz" -C "$temp_dir"
-
 echo "=> Launch install-script"
 cd $temp_dir
 cd geox-tweak-xfce-master
