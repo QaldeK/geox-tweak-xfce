bf829b0e78c45951ee512d2aa556546038d706ec
QaldeK <aldek@vivaldi.net>
2020-05-01 11:35:18 +0000

    papirus-folder launch

--- geox-tweak/geox_tweak_xfce.py
+++ geox-tweak/geox_tweak_xfce.py
@@ -178,7 +178,7 @@ class GeoxTweak:
         # import pdb; pdb.set_trace()
         script = '''#!/bin/bash
 Theme=$(xfconf-query -c xsettings -p /Net/IconThemeName)
-xfce4-terminal -e "sudo papirus-folders -t $Theme -C ''' + cfolder + ''' ''''
+xfce4-terminal -e "sudo papirus-folders -t $Theme -C ''' + cfolder + '''--hold" '''
 
         subprocess.Popen(script, shell=True)
 
