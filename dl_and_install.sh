db291afca3c13cb32044ef9cae29eec8a1a2b799
qaldek <aldek@vivaldi.net>
2020-05-01 03:57:16 +0200

    ..

--- dl_and_install.sh
+++ dl_and_install.sh
@@ -3,10 +3,8 @@
 gh_repo="geox-tweak-xfce"
 temp_dir="$(mktemp -d)"
 
-
 echo "=> Getting the latest version from GitHub ..."
-wget -O "/tmp/$gh_repo.tar.gz" \
-https://github.com/QaldeK/geox-tweak-xfce/archive/master.tar.gz
+wget -O "/tmp/$gh_repo.tar.gz" https://github.com/QaldeK/geox-tweak-xfce/archive/master.tar.gz
 
 echo "=> Unpacking archive ..."
 tar -xzf "/tmp/$gh_repo.tar.gz" -C "$temp_dir"
